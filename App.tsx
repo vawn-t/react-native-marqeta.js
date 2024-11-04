4a4fb3f1ef49640df759ef98de433838c8ba5bde
vawn <vantran99dn@gmail.com>
2024-11-04 14:34:20 +0700

    Add new dependencies and update project configuration for React Native

--- App.tsx
+++ App.tsx
@@ -6,113 +6,48 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Alert, Platform, SafeAreaView} from 'react-native';
+import ReactNativeBlobUtil from 'react-native-blob-util';
+import WebView from 'react-native-webview';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const appDirectories = ReactNativeBlobUtil.fs.dirs;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  let htmlFilePath = '';
+  if (Platform.OS === 'ios') {
+    htmlFilePath = `${appDirectories.MainBundleDir}/sensitive.html`;
+  } else {
+    htmlFilePath = 'file:///android_asset/sensitive.html';
+  }
+
+  const token =
+    'eyJ0b2tlbiI6IjAyODYyZTU1LWE4MGUtNDVmYy1iMzhlLTFkYmNlODQxODJhMiZhcHBsaWNhdGlvbl90b2tlbj10cnByaW1lX3NhbmRib3hfYXBpX2NvbnN1bWVyIiwiYXBwbGljYXRpb24iOnsidG9rZW4iOiJ0cnByaW1lX3NhbmRib3hfYXBpX2NvbnN1bWVyIiwiYWN0aXZlIjp0cnVlLCJjbGllbnRfYXBpX2Jhc2VfdXJsIjoiaHR0cHM6Ly93aWRnZXRzLXNhbmRib3gubWFycWV0YS5jb20vY2xpZW50L2FwaS92MSIsImFzc2V0c191cmwiOiJodHRwczovL3dpZGdldHMtc2FuZGJveC5tYXJxZXRhLmNvbS9jbGllbnQvYXNzZXRzLzEuMC4wIn0sImFwcGxpY2F0aW9uX3Rva2VuIjoidHJwcmltZV9zYW5kYm94X2FwaV9jb25zdW1lciIsImNsaWVudHRva2VuX2lkIjoiNjcyODc1NjRlY2E1YmE3NDIyMDQzODJjIiwiY2FyZF90b2tlbiI6InRwcmltZV9jYXJkXzExa2hoOGo4MTJjcmVtMSIsImNyZWF0ZWQiOiIyMDI0LTExLTA0VDA3OjE5OjAwKzAwMDAiLCJleHBpcmVzIjoiMjAyNC0xMS0wNFQwNzoyNDowMCswMDAwIn0=';
+
+  const externalURL =
+    'https://widgets.marqeta.com/marqetajs/2.0.0/marqeta.min.js'; // NOTE: Replace with your external URL if env is production
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={{flex: 1}}>
+      <WebView
+        source={{uri: htmlFilePath}}
+        scrollEnabled={false}
+        scalesPageToFit
+        allowFileAccessFromFileURLs
+        javaScriptEnabled
+        domStorageEnabled
+        allowUniversalAccessFromFileURLs
+        allowFileAccess
+        originWhitelist={['*']}
+        mixedContentMode="always"
+        injectedJavaScriptObject={{token, externalURL}} // Pass token and the external url to htmlView
+        onMessage={event => {
+          Alert.alert('Message from WebView: ' + event.nativeEvent.data);
+        }}
+        style={{flex: 1}}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
